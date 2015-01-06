b464a180422a130bb86ecba876b1ef948cd062e1
Anting Shen <shenantingtt@hotmail.com>
2015-01-05 22:26:31 -0600

    Implemented PWM

--- MSP430/main.c
+++ MSP430/main.c
@@ -1,5 +1,10 @@
 #include <msp430g2553.h>
 
+#define LMOTOR_DUTY TA1CCR1
+#define RMOTOR_DUTY TA1CCR2
+#define LMOTOR_PIN BIT1
+#define RMOTOR_PIN BIT4
+
 #define SDA_PIN BIT7
 #define SCL_PIN BIT6
 #define RGB_ADDRESS	0x62
@@ -71,7 +76,20 @@ void lcd_write(unsigned char addr, char* string, int bytes) {
 	i2c_data = (unsigned char*)string;
 	i2c_mode = LCD_WRITE_MODE;
 	UCB0CTL1 |= UCTXSTT;
-	LPM0;
+	LPM0; // Remove..
+}
+
+void pwm_init() {
+	TA1CCR0 = 100;
+	TA1CCTL1 = OUTMOD_7;
+	TA1CCTL2 = OUTMOD_7;
+	LMOTOR_DUTY = 0xffff;		// Start as off
+	RMOTOR_DUTY = 0xffff;
+	P2DIR = LMOTOR_PIN | RMOTOR_PIN;
+	P2SEL = LMOTOR_PIN | RMOTOR_PIN;
+	TA1CTL = TASSEL_2	// Use SMCLK (system clock)
+			| MC_1		// Timer counts up to TACCR0
+			| ID_3;		// Divide clock input by 8
 }
 
 int main(void) {
@@ -79,11 +97,19 @@ int main(void) {
     __enable_interrupt();
 
     P1DIR |= BIT0;
+    P3REN = ~0;
 
 	i2c_init();
 	lcd_init();
 	lcd_write(LCD_BOT_LINE, "I am a catlol", 10);
-	LPM0;
+
+	pwm_init();
+	LMOTOR_DUTY = 10;
+	RMOTOR_DUTY = 90;
+
+	for (;;) {
+		LPM0;
+	}
 }
 
 #pragma vector = USCIAB0TX_VECTOR
