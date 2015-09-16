23e2d6f29422f2c9aa87fa072eb4d4c0440b3338
David.Hahn <ddavidhahn@gmail.com>
2015-09-16 12:41:08 -0700

    Updated unstucker and parachute to scope global variables and added tester code to unstucker.

--- Edison/parachute.js
+++ Edison/parachute.js
@@ -1,15 +1,17 @@
 //TODO: Have a hard coded start time (e.g. wait 15 minutes and then start)
-
-var GPS_THRESHOLD_ALTITUDE = 99; // Needs to be adjusted: threshold for difference between previous and current gps reading values for altitude
-var GPS_THRESHOLD_LAT_LON = 10; // Needs to be adjusted: threshold for difference between previous and current gps reading values for latitude and longitude
-var GPS_AVERAGE_THRESHOLD_ALTITUDE = 50; // Needs to be adjusted: threshold for difference between average of last 10 gps readings and current gps reading
-var GPS_AVERAGE_THRESHOLD_LAT_LON = 50; // Needs to be adjusted: threshold below which we believe our lat lon values are not changing enough: we are stuck
-//var ACCELEROMETER_THRESHOLD = 10; //Needs to be adjusted: threshold for difference between accelerometer values
-var TOTAL_VALUES_TO_STORE = 10;
-var RELEASE_UNDER_VALUE = 1200;
-var IGNORE_CURR_GPS_BELOW = 700;//Value under which we consider GPS readings to be too low and incorrect data
-var RUNNING_TEST_CODE = 0; //Gets set to 1 if we run testParachute()
-var LANDED = 0;
+var PARACHUTE_VARIABLES = {
+    GPS_THRESHOLD_ALTITUDE : 100, // Needs to be adjusted: threshold for difference between previous and current gps reading values for altitude
+    GPS_THRESHOLD_LAT_LON : 100, // Needs to be adjusted: threshold for difference between previous and current gps reading values for latitude and longitude
+    GPS_AVERAGE_THRESHOLD_ALTITUDE : 50, // Needs to be adjusted: threshold for difference between average of last 10 gps readings and current gps reading
+    GPS_AVERAGE_THRESHOLD_LAT_LON : 10, // Needs to be adjusted: threshold below which we believe our lat lon values are not changing enough: we are stuck
+    //ACCELEROMETER_THRESHOLD = 10, //Needs to be adjusted: threshold for difference between accelerometer values
+    TOTAL_VALUES_TO_STORE : 10,
+    RELEASE_UNDER_VALUE : 1200,
+    IGNORE_CURR_GPS_BELOW : 700, //Value under which we consider GPS readings to be too low and incorrect data
+    RUNNING_TEST_CODE : 0, //Gets set to 1 if we run testParachute()
+    LANDED : 0,
+    TIME_TO_WAIT_BEFORE_SAMPLING : 900000
+}
 
 var checkServo = function(cb) {
     var readyToRelease = false;
@@ -18,31 +20,32 @@ var checkServo = function(cb) {
     var cb = cb||releaseParachute;
 
     var interval = setInterval(function() {
-        if (buffering) {
+        if (SAMPLING_VARIABLES.buffering) {
             return;
         }
 
-        if (RUNNING_TEST_CODE) {
+        if (PARACHUTE_VARIABLES.RUNNING_TEST_CODE) {
             currGPSAlt = getGPSTest().altitude;
         } else {
             currGPSAlt = getGPS().altitude;
         }
 
-        console.log("Waiting to get below RELEASE_UNDER_VALUE. Current value is: " + currGPSAlt);
+        console.log("Waiting to get below PARACHUTE_VARIABLES.RELEASE_UNDER_VALUE. Current value is: " + currGPSAlt);
 
-        if (currGPSAlt < RELEASE_UNDER_VALUE && currGPSAlt > IGNORE_CURR_GPS_BELOW) {
+        if (currGPSAlt < PARACHUTE_VARIABLES.RELEASE_UNDER_VALUE && currGPSAlt > PARACHUTE_VARIABLES.IGNORE_CURR_GPS_BELOW) {
             console.log("We think we are on the ground. Start other checks.");
             if (!idToClear) {
                 idToClear = setInterval(sampleGPS, 2000);
             }
-            if (!(Math.abs(gpsAltitudeAverage - currGPSAlt) > GPS_AVERAGE_THRESHOLD_ALTITUDE || gpsValues.length < TOTAL_VALUES_TO_STORE)) {
+            if (!(Math.abs(SAMPLING_VARIABLES.gpsAltitudeAverage - currGPSAlt) > PARACHUTE_VARIABLES.GPS_AVERAGE_THRESHOLD_ALTITUDE || SAMPLING_VARIABLES.gpsValues.length < PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE)) {
                 clearInterval(idToClear);
                 //sampleAccelerometer(function(val) {
                 //    if(val) {
                 //        clearInterval(idToClear);
                         console.log("Releasing Parachute");
                         clearInterval(interval);
-                        LANDED = 1;
+                        PARACHUTE_VARIABLES.LANDED = 1;
+                        writeToParachuteReleased(1);
                         cb();
                 //    }
                 //})
@@ -51,70 +54,72 @@ var checkServo = function(cb) {
     }, 2000)
 }
 
-var gpsValues = [];
-var gpsLonAverage = 0;
-var gpsLonTotal = 0;
-var gpsLatAverage = 0;
-var gpsLatTotal = 0;
-var gpsAltitudeAverage = 0;
-var gpsAltitudeTotal = 0;
-var buffering = 0;
+var SAMPLING_VARIABLES = {
+    gpsValues : [],
+    gpsLonAverage : 0,
+    gpsLonTotal : 0,
+    gpsLatAverage : 0,
+    gpsLatTotal : 0,
+    gpsAltitudeAverage : 0,
+    gpsAltitudeTotal : 0,
+    buffering : 0,
+}
 var sampleGPS = function() {
     console.log("Calling sampleGPS");
 
     //Sampling procedure for first time values are collected
-    if(gpsValues.length == 0 && !buffering) {
+    if(SAMPLING_VARIABLES.gpsValues.length == 0 && !SAMPLING_VARIABLES.buffering) {
         bufferGPSValues();
         return;
     }
 
-    if (buffering) {
+    if (SAMPLING_VARIABLES.buffering) {
         return;
     }
 
     //Otherwise proceed with regular single GPS entry sampling
-    var prevVal = gpsValues[gpsValues.length - 1];
+    var prevVal = SAMPLING_VARIABLES.gpsValues[SAMPLING_VARIABLES.gpsValues.length - 1];
     var currVal;
-    if (RUNNING_TEST_CODE) {
+    if (PARACHUTE_VARIABLES.RUNNING_TEST_CODE) {
         currVal = copyGPSObject(getGPSTest());
     } else {
         currVal = copyGPSObject(getGPS());
     }
     var poppedVal;
 
-    if (!LANDED) {
-        if (Math.abs(currVal.altitude - prevVal.altitude) > GPS_THRESHOLD_ALTITUDE) {
-            gpsValues = [];
+    if (!PARACHUTE_VARIABLES.LANDED) {
+        if (Math.abs(currVal.altitude - prevVal.altitude) > PARACHUTE_VARIABLES.GPS_THRESHOLD_ALTITUDE) {
+            SAMPLING_VARIABLES.gpsValues = [];
             return;
         }
     } else {
-        if (Math.abs(currVal.longitude - prevVal.longitude) > GPS_THRESHOLD_LAT_LON || Math.abs(currVal.latitude - prevVal.latitude) > GPS_THRESHOLD_LAT_LON) {
-            gpsValues = [];
+        if (Math.abs(currVal.longitude - prevVal.longitude) > PARACHUTE_VARIABLES.GPS_THRESHOLD_LAT_LON || Math.abs(currVal.latitude - prevVal.latitude) > PARACHUTE_VARIABLES.GPS_THRESHOLD_LAT_LON) {
+            SAMPLING_VARIABLES.gpsValues = [];
             return;
         }
     }
 
-    gpsValues.push(currVal);
-    if (gpsValues.length > TOTAL_VALUES_TO_STORE) {
-        var oldestGPSValue = gpsValues[0];
-        gpsValues = gpsValues.slice(1, TOTAL_VALUES_TO_STORE + 1);
-        gpsLatTotal = gpsLatTotal - oldestGPSValue.latitude + currVal.latitude;
-        gpsLonTotal = gpsLonTotal - oldestGPSValue.longitude + currVal.longitude;
-        gpsAltitudeTotal = gpsAltitudeTotal - oldestGPSValue.altitude + currVal.altitude;
+    SAMPLING_VARIABLES.gpsValues.push(currVal);
+    if (SAMPLING_VARIABLES.gpsValues.length > PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE) {
+        var oldestGPSValue = SAMPLING_VARIABLES.gpsValues[0];
+        SAMPLING_VARIABLES.gpsValues = SAMPLING_VARIABLES.gpsValues.slice(1, PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE + 1);
+        SAMPLING_VARIABLES.gpsLatTotal = SAMPLING_VARIABLES.gpsLatTotal - oldestGPSValue.latitude + currVal.latitude;
+        SAMPLING_VARIABLES.gpsLonTotal = SAMPLING_VARIABLES.gpsLonTotal - oldestGPSValue.longitude + currVal.longitude;
+        SAMPLING_VARIABLES.gpsAltitudeTotal = SAMPLING_VARIABLES.gpsAltitudeTotal - oldestGPSValue.altitude + currVal.altitude;
     } else {
-        gpsLatTotal += currVal.latitude;
-        gpsLonTotal += currVal.longitude;
-        gpsAltitudeTotal += currVal.altitude;
+        SAMPLING_VARIABLES.gpsLatTotal += currVal.latitude;
+        SAMPLING_VARIABLES.gpsLonTotal += currVal.longitude;
+        SAMPLING_VARIABLES.gpsAltitudeTotal += currVal.altitude;
     }
-    gpsLatAverage = gpsLatTotal / TOTAL_VALUES_TO_STORE;
-    gpsLonAverage = gpsLonTotal / TOTAL_VALUES_TO_STORE;
-    gpsAltitudeAverage = gpsAltitudeTotal / TOTAL_VALUES_TO_STORE;
-    console.log(gpsValues);
+    SAMPLING_VARIABLES.gpsLatAverage = SAMPLING_VARIABLES.gpsLatTotal / PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE;
+    SAMPLING_VARIABLES.gpsLonAverage = SAMPLING_VARIABLES.gpsLonTotal / PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE;
+    SAMPLING_VARIABLES.gpsAltitudeAverage = SAMPLING_VARIABLES.gpsAltitudeTotal / PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE;
+    console.log(SAMPLING_VARIABLES.gpsValues);
 }
 
 var bufferDone = 0;
 var bufferGPSValues = function() {
-    buffering = 1;
+    SAMPLING_VARIABLES.buffering = 1;
     bufferDone = 0;
     var GPSbuffer = [];
     var totalLoops = 3;
@@ -133,7 +138,7 @@ var bufferGPSValues = function() {
 
         console.log("Grabbing entry for GPS buffer.");
         //console.log("Counter: " + counter);
-        if (RUNNING_TEST_CODE) {
+        if (PARACHUTE_VARIABLES.RUNNING_TEST_CODE) {
             GPSbuffer.push(copyGPSObject(getGPSTest()));
             console.log("Buffer after V");
             console.log(GPSbuffer);
@@ -150,30 +155,31 @@ var bufferGPSValues = function() {
                 var entry;
                 for (var i = 0; i < GPSbuffer.length; i++) {
                     entry = GPSbuffer[i];
-                    gpsValues.push(entry);
-                    gpsLonTotal += entry.latitude;
-                    gpsLatTotal += entry.longitude;
-                    gpsAltitudeTotal += entry.altitude;
+                    SAMPLING_VARIABLES.gpsValues.push(entry);
+                    SAMPLING_VARIABLES.gpsLonTotal += entry.latitude;
+                    SAMPLING_VARIABLES.gpsLatTotal += entry.longitude;
+                    SAMPLING_VARIABLES.gpsAltitudeTotal += entry.altitude;
                 }
-                console.log(gpsValues);
+                console.log(SAMPLING_VARIABLES.gpsValues);
             }
-            buffering = 0;
+            SAMPLING_VARIABLES.buffering = 0;
             clearInterval(checkerID);
         }    
     }, 2000);
     return;
 }
 
+//Ensure that buffered values are close enough to each other.
 var checkValidBufferValues = function(listOfPotentialValues) {
     for (var i = 0; i < listOfPotentialValues.length; i++) {
         for (var j = 0; j < listOfPotentialValues.length; j++) {
             if (i != j) {
-                if (!LANDED) {
-                    if (Math.abs(currVal.altitude - prevVal.altitude) > GPS_THRESHOLD_ALTITUDE) {
+                if (!PARACHUTE_VARIABLES.LANDED) {
+                    if (Math.abs(currVal.altitude - prevVal.altitude) > PARACHUTE_VARIABLES.GPS_THRESHOLD_ALTITUDE) {
                         return false;
                     }
                 } else {
-                    if (Math.abs(currVal.longitude - prevVal.longitude) > GPS_THRESHOLD_LAT_LON || Math.abs(currVal.latitude - prevVal.latitude) > GPS_THRESHOLD_LAT_LON) {
+                    if (Math.abs(currVal.longitude - prevVal.longitude) > PARACHUTE_VARIABLES.GPS_THRESHOLD_LAT_LON || Math.abs(currVal.latitude - prevVal.latitude) > PARACHUTE_VARIABLES.GPS_THRESHOLD_LAT_LON) {
                         return false;
                     }
                 }
@@ -189,6 +195,13 @@ var copyGPSObject = function(objectToCopy) {
     copy.longitude = objectToCopy.longitude;
     copy.altitude = objectToCopy.altitude;
     return copy;
+}
+
+var fs = require('fs');
+var writeToParachuteReleased = function(text) {
+    fs.writeFile('/home/root/parachuteReleased', text, function(err) {
+        if (err) throw err;
+    });
 }
 
 /*var sampleAccelerometer = function(cb) {
@@ -222,7 +235,7 @@ var counter = 0;
 
 //Run this to test parachute code
 var testParachuteWithDummyCode = function() {
-    RUNNING_TEST_CODE = 1;
+    PARACHUTE_VARIABLES.RUNNING_TEST_CODE = 1;
     setGPSTestData(0, 0, currAlti);
 
     var idClear = setInterval(function(cb) {
@@ -239,7 +252,6 @@ var testParachuteWithDummyCode = function() {
     checkServo(function() {
         clearInterval(idClear);
         releaseParachute();
-        RUNNING_TEST_CODE = 0;
+        PARACHUTE_VARIABLES.RUNNING_TEST_CODE = 0;
     });
 }
-
--- Edison/unstucker.js
+++ Edison/unstucker.js
@@ -1,86 +1,163 @@
-var ROTATIONAL_TRIES = 10;
-var TURN_TRIES = 5;
-var ADJUSTMENT_THRESHOLD = 5;
-var WIGGLE_TRIES = 10;
+var UNSTUCKER_VARIABLES = {
+    rotateCount : 0,
+    ROTATIONAL_TRIES : 10,
+    WIGGLE_TRIES : 5,
+}
 
 var isStuck = function() {
-    if (gpsValues.length < TOTAL_VALUES_TO_STORE) {
+    console.log("Checking stuckness");
+    if (SAMPLING_VARIABLES.gpsValues.length < PARACHUTE_VARIABLES.TOTAL_VALUES_TO_STORE) {
+        console.log("Still stuck. Not enough data.");
         return false;
     } else {
         var currGPSVal;
-        if (RUNNING_TEST_CODE) {
+        if (PARACHUTE_VARIABLES.RUNNING_TEST_CODE) {
             currGPSVal = copyGPSObject(getGPSTest());
         } else {
             currGPSVal = copyGPSObject(getGPS());
         }
 
-        if (Math.abs(gpsLonAverage - currGPSVal.longitude) < GPS_AVERAGE_THRESHOLD_LAT_LON || Math.abs(gpsLatAverage - currGPSVal.latitude) < GPS_AVERAGE_THRESHOLD_LAT_LON) {
+        if (Math.abs(SAMPLING_VARIABLES.gpsLonAverage - currGPSVal.longitude) < PARACHUTE_VARIABLES.GPS_AVERAGE_THRESHOLD_LAT_LON && Math.abs(SAMPLING_VARIABLES.gpsLatAverage - currGPSVal.latitude) < PARACHUTE_VARIABLES.GPS_AVERAGE_THRESHOLD_LAT_LON) {
+            console.log("Still stuck.");
             return true;
         } else {
+            console.log("Not stuck anymore. We should be able to move again.");
             return false;
         }
     }
 }
 
+var testUnstucker = function() {
+    PARACHUTE_VARIABLES.RUNNING_TEST_CODE = 1;
+    changeGPSValues();
+    resampleGPSValues();
+    console.log(isStuck());
+}
+
+var changeGPSValues = function() {
+    SAMPLING_VARIABLES.gpsValues = [
+        {latitude : 50, longitude : 50, altitude : 0},
+        {latitude : 60, longitude : 60, altitude : 0},
+        {latitude : 70, longitude : 70, altitude : 0},
+        {latitude : 80, longitude : 50, altitude : 0},
+        {latitude : 90, longitude : 50, altitude : 0},
+        {latitude : 100, longitude : 50, altitude : 0},
+        {latitude : 110, longitude : 50, altitude : 0},
+        {latitude : 110, longitude : 60, altitude : 0},
+        {latitude : 115, longitude : 65, altitude : 0},
+        {latitude : 125, longitude : 55, altitude : 0},
+    ];
+}
+
+var resampleGPSValues = function() {
+    var totalLat = 0;
+    var totalLon = 0;
+    for (element in SAMPLING_VARIABLES.gpsValues) {
+        totalLat += SAMPLING_VARIABLES.gpsValues[element].latitude;
+        totalLon += SAMPLING_VARIABLES.gpsValues[element].longitude;
+    }
+    SAMPLING_VARIABLES.gpsLonAverage = totalLon / SAMPLING_VARIABLES.gpsValues.length;
+    SAMPLING_VARIABLES.gpsLatAverage = totalLat / SAMPLING_VARIABLES.gpsValues.length;
+}
+
 var rotateClockwise = function() {
-    driveMotors(100, -100);
-    //Needs timeout time
+    console.log("Rotating clockwise");
+    //driveMotors(100, -100);
 }
 
 var rotateCounterClockwise = function() {
-    driveMotors(-100, 100);
-    //Needs timeout time
+    console.log("Rotating counter clockwise");
+    //driveMotors(-100, 100);
 }
 
 var goBackwards = function() {
-    driveMotors(-100, -100);
-    //Needs timeout time
+    console.log("Moving backwards");
+    //driveMotors(-100, -100);
 }
 
 var goForwards = function() {
-    driveMotors(100, 100);
-    //Needs timeout time
+    console.log("Moving forwards");
+    //driveMotors(100, 100);
+}
+
+var stopMotors = function() {
+    console.log("Braking");
+    //driveMotors(0, 0);
 }
 
 var wiggle = function() {
-    for(var i = 0; i < WIGGLE_TRIES; i++)
+    for(var i = 0; i < UNSTUCKER_VARIABLES.WIGGLE_TRIES; i++)
     {
         driveMotors(100, 50);
-        //Needs timeout time
-        driveMotors(50, 100);
-        //Needs timeout time
-        if(!isStuck()) {
-            return;
-        }
+        setTimeout(function() {
+            driveMotors(50, 100);
+        }, 2000);
+        setTimeout(stopMotors, 4000);
     }
 }
 
-var unstuckProcedure = function(initialLeft, initialRight) {
-    for(var j = 0; j < ROTATIONAL_TRIES; j++) {
-        for(var i = 0; !isStuck() && i < TURN_TRIES; i++) {
-            goBackwards();
-            goForwards(initial_left, initial_right);
+var unstuckProcedureClockwise = function(cb) {
+    console.log("Initiated unstuckProcedureClockwise");
+    UNSTUCKER_VARIABLES.rotateCount = 0;
+
+    var idToClear = setInterval(function() {
+        console.log("I'm inside!");
+        if (UNSTUCKER_VARIABLES.rotateCount >= UNSTUCKER_VARIABLES.ROTATIONAL_TRIES) {
+            clearInterval(idToClear);
+            unstuckProcedureCounterClockwise();
+            if (cb) {
+                cb();
+            }
         }
 
-        if(isStuck()) {
-            rotateClockwise();
-            wiggle();
-        } else {
-            return;
-        }
-    }
+        goBackwards();
+        setTimeout(goForwards, 5000);
+        setTimeout(function() {
+            stopMotors();
 
-    for(var j = 0; j < 2 * ROTATIONAL_TRIES; j++) {
-        for(var i = 0; !isStuck() && i < TURN_TRIES; i++) {
-            goBackwards();
-            goForwards(initial_left, initial_right);
+            if (!isStuck()) {
+                clearInterval(idToClear);
+                if (cb) {
+                    cb();
+                }
+            } else {
+                rotateClockwise();
+                setTimeout(stopMotors, 500);
+                //wiggle();
+                UNSTUCKER_VARIABLES.rotateCount += 1;
+            }
+        }, 15000);
+    }, 20000 + (4000 * UNSTUCKER_VARIABLES.WIGGLE_TRIES));
+}
+
+var unstuckProcedureCounterClockwise = function(cb) {
+    console.log("Initiated unstuckProcedureCounterClockwise");
+    UNSTUCKER_VARIABLES.rotateCount = 0;
+
+    var idToClear = setInterval(function() {
+        if (UNSTUCKER_VARIABLES.rotateCount >= UNSTUCKER_VARIABLES.ROTATIONAL_TRIES) {
+            clearInterval(idToClear);
+            if (cb) {
+                cb();
+            }
         }
 
-        if(isStuck()) {
-            rotateCounterClockwise();
-            wiggle();
-        } else {
-            return;
-        }
-    }
+        goBackwards();
+        setTimeout(goForwards, 5000);
+        setTimeout(function() {
+            stopMotors();
+
+            if (!isStuck()) {
+                clearInterval(idToClear);
+                if (cb) {
+                    cb();
+                }
+            } else {
+                rotateCounterClockwise();
+                setTimeout(stopMotors, 500);
+                //wiggle();
+                UNSTUCKER_VARIABLES.rotateCount += 1;
+            }
+        }, 15000);
+    }, 20000 + (4000 * UNSTUCKER_VARIABLES.WIGGLE_TRIES));
 }
